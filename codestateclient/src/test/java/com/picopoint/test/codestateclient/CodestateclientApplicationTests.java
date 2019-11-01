23dce3bb68a1ca0ad3567f941c0fe16cba313b89
Bas van Welzen <bas@picopoint.com>
2019-11-01 13:14:39 +0100

    Initial commit

---
+++ codestateclient/src/test/java/com/picopoint/test/codestateclient/CodestateclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picopoint.test.codestateclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodestateclientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
